9c2637aede3cf895ddc7af3dedd9ee385b519862
Abdirizak <abdirizaksigat99@gmail.com>
2023-01-22 01:24:21 -0800

    added product screen

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import { Axios } from 'axios';
+import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, product: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 export default function ProductScreen() {
   const params = useParams();
   const { slug } = params;
-  return (
-    <div>
-      <h1>{slug}</h1>
-    </div>
-  );
+
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    product: [],
+    loading: true,
+    error: '',
+  });
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await Axios.get(
+          `http://localhost:5000/api/products/slug/${slug}`
+        );
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+      // setProducts(result.data);
+    };
+    fetchData();
+  }, [slug]);
+
+  return <div>{slug}</div>;
 }
