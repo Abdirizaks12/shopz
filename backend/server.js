f6d4d2d4822daf1a23148a41caf11b5b3998429d
Abdirizak <abdirizaksigat99@gmail.com>
2023-01-30 04:23:15 -0800

    worked on add to cart functionality

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,14 @@ app.get('/api/products/slug/:slug', (req, res) => {
   }
 });
 
+app.get('/api/products/:id', async (req, res) => {
+  const product = await data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
