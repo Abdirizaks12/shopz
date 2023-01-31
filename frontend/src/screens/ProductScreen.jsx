c39ae9731e9a2821d9ec42c9d9160e5fbf2e974e
Abdirizak <abdirizaksigat99@gmail.com>
2023-01-31 10:37:46 -0800

    fixed the add to  cart function

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -65,7 +65,7 @@ export default function ProductScreen() {
     }
     ctxDispatch({
       type: 'CART_ADD_ITEM',
-      payload: { ...product, quantity: 1 },
+      payload: { ...product, quantity },
     });
   };
   return loading ? (
