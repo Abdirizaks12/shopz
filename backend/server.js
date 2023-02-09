38524da98c68b94a2cd29cdb48924c87a40a6172
Abdirizak <abdirizaksigat99@gmail.com>
2023-02-09 10:54:27 +0300

    feat:installed MONGO DB

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,18 @@
-// import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-// dotenv.config();
-// import express from 'express';
-// import cors from 'cors';
-// import data from './data.js';
-// const app = express();
-// app.use(cors());
-// const port = process.env.PORT | 5000;
-
-// app.get('/api/products', async (req, res) => {
-//   try {
-//     await res.send(data.products);
-//     console.log('data =', data.products);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-// app.listen(port, () => console.log(`app listen on port ${port}`));
-
 import express from 'express';
 import data from './data.js';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 app.use(cors());
