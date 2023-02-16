49bdb75c405ff08434a9336dd89601520f5db71d
Abdirizak <abdirizaksigat99@gmail.com>
2023-02-16 16:46:40 +0300

    feat:added a seed sample product

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import data from './data.js';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoute.js';
+import productRouter from './routes/productRouter.js';
 
 dotenv.config();
 mongoose
@@ -15,27 +17,10 @@ mongoose
   });
 
 const app = express();
+app.use('/api/seed', seedRouter);
 app.use(cors());
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
+app.use('/api/products', productRouter);
 
-app.get('/api/products/:id', async (req, res) => {
-  const product = await data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
